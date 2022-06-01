f1ff9e31bd657c72ee3c90adf050e259a20c1465
Legrain Samuel <samuel.legrain@bstorm.be>
2022-06-01 16:46:32 +0200

    Ajoutez des fichiers projet.

---
+++ Demo-SecondApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Demo_SecondApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Veuillez indiquer votre nom :");
+            string name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("Et quel est votre âge ?");
+            string age = Console.ReadLine();
+            Console.Clear();
+            //Console.Write("J'ai ");
+            //Console.Write(34);
+            //Console.Write(" ans!");
+            Console.Write("Bonjour {0}, vous avez {1} ans!", name, age);
+        }
+    }
+}
