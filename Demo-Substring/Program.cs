e17f12adb7b7a34aaa7d96cfa7de0e5fe1c3e711
Legrain Samuel <samuel.legrain@bstorm.be>
2022-06-21 09:48:19 +0200

    Add demo Substring

---
+++ Demo-Substring/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo_Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!";
+            Console.WriteLine(message.Substring(0,5));
+            Console.WriteLine(message.Substring(message.Length-6,6));
+            //Console.WriteLine(message.Substring(message.Length-6,7));     //Dépassement des valeurs du texte : Crash ArgumentOutOfRangeException
+        }
+    }
+}
