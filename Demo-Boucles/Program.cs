452e426ca5c5e5b69cea45ba52e2883cff079c5f
Legrain Samuel <samuel.legrain@bstorm.be>
2022-06-27 12:10:56 +0200

    Add Demos Boucles & Tableau

---
+++ Demo-Boucles/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Demo_Boucles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Boucle for(...;...;...)
+            #region Arithmétique
+            //for (int i = 0; i < 10; i+=2)
+            //{
+            //    Console.ForegroundColor = (ConsoleColor)i;
+            //    Console.WriteLine(i);
+            //}
+            ////Console.WriteLine(i); //i est déclaré dans les parenthèse du for, donc pas accessible depuis l'extérieur
+            #endregion
+            #region non arithmétique
+
+            //Console.WriteLine("Veuillez indiquer un numéro :");
+            //bool isOk = true;
+            //for (string input = Console.ReadLine(); !int.TryParse(input, out _) && isOk; input = Console.ReadLine())
+            //{
+            //    Console.WriteLine("Un numéro, tu sais ce que c'est?");
+            //    isOk = Console.ReadLine() == "Oui";
+            //}
+            //Console.WriteLine("Ok, c'est bon pour aujourd'hui, tu m'épuises...");
+            #endregion
+            #endregion
+
+            #region while(...)
+            #region Arthimétique
+
+            //int x = 0;
+
+            //while (x < 10)
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+
+            //Console.WriteLine(x);
+
+            #endregion
+            #region non-arithmétique
+
+            //Console.WriteLine("Veuillez entrer un nombre :");
+            //string input = Console.ReadLine();
+            //int i;
+
+            //while(!int.TryParse(input, out i) || i > 100)
+            //{
+            //    Console.WriteLine("Veuillez entrer un nombre correct :");
+            //    input = Console.ReadLine();
+            //}
+
+            //Console.WriteLine($"Le nombre {i} est un nombre...");
+            #endregion
+            #endregion
+
+            #region do{ ... } while(...);
+
+            string input;
+            int i;
+            string message = "Veuillez entrer un nombre";
+            do
+            {
+                Console.WriteLine(message);
+                input = Console.ReadLine();
+                message = "Veuillez entrer un nombre correct...";
+            } while (!int.TryParse(input, out i));
+
+            Console.WriteLine($"Le nombre {i} est un nombre...");
+
+            #endregion
+        }
+    }
+}
---
+++ Demo-Tableau/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Demo_Tableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] jourSemaine = { "Lu", "Ma", "Me", "Je", "Ve", "Sa", "Di" };
+            // Les indices du tableau :   0      1      2     3      4      5      6
+
+            //Aujourd'hui nous somme Lundi, pour obtenir la bon jour, il me faut utiliser l'index 0 (Première du tableau)
+            Console.WriteLine($"Aujourd'hui nous somme {jourSemaine[0]}");
+
+            string[,] jourSemaineMultilangue =
+                {
+                    { "Lu", "Ma", "Me", "Je", "Ve", "Sa", "Di" },
+// Les indices du 0:      0      1      2     3      4      5      6
+                    { "Ma", "Di", "Wo", "Do", "Vr", "Za", "Zo" },
+// Les indices du 1:      0      1      2     3      4      5      6
+                    { "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"}
+// Les indices du 2:      0      1      2     3      4      5      6
+                };
+
+            //Je veux dire en anglais que demain nous sommes Tuesday, il me faut repérer l'indice de la langue (premier axe), suivi, de l'indice du jour (seconde axe) :
+
+            Console.WriteLine($"Tomorrow, we are {jourSemaineMultilangue[2,1]}!");
+
+            string[,,] jourSemaineMultiFormat =
+                {
+                    {
+                        { "Lu", "Ma", "Me", "Je", "Ve", "Sa", "Di" },
+    // Les indices du 0:      0      1      2     3      4      5      6
+                        { "Ma", "Di", "Wo", "Do", "Vr", "Za", "Zo" },
+    // Les indices du 1:      0      1      2     3      4      5      6
+                        { "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"}
+    // Les indices du 2:      0      1      2     3      4      5      6
+                    },
+                    {
+                        { "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim" },
+    // Les indices du 0:      0      1      2     3      4      5      6
+                        { "Maa", "Din", "Woe", "Don", "Vri", "Zat", "Zon" },
+    // Les indices du 1:      0      1      2     3      4      5      6
+                        { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}
+    // Les indices du 2:      0      1      2     3      4      5      6
+                    },
+                    {
+                        { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" },
+    // Les indices du 0:      0      1      2     3      4      5      6
+                        { "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag" },
+    // Les indices du 1:      0      1      2     3      4      5      6
+                        { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saterday", "Sunday"}
+    // Les indices du 2:      0      1      2     3      4      5      6
+                    }
+                };
+
+            //Dire en Français que mon jour préféré est vendredi, car tout est permis...
+            Console.WriteLine($"J'aime le {jourSemaineMultiFormat[2,0,4]}, car tout est permis!");
+
+            Dictionary<string, bool>
+        }
+    }
+}
