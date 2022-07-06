7b27fb51b8f199b5e981e93df823f4746ae04543
Legrain Samuel <samuel.legrain@bstorm.be>
2022-07-06 16:37:58 +0200

    Add Demo Récursive

---
+++ Demo-Recursive/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Demo_Recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogging parcours1;
+            parcours1.Courir(4);
+        }
+    }
+
+    public struct Jogging
+    {
+        public void Courir(int distance)
+        {
+            distance = distance - 1;
+            Console.WriteLine($"Il me reste {distance} m à courir.");
+            if (distance == 0) return;
+            this.Courir(distance);
+            Console.WriteLine($"Je rentre chez moi, j'ai marché {distance} m.");
+        }
+    }
+}
